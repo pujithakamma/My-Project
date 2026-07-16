80b72c6ddaa36c054e137782f0fedc008a66f7c0
pujitha <23ht1a4342@gmail.com>
2026-07-16 12:01:34 +0530

    Update server.js

--- Farm-Connect-Backend/server.js
+++ Farm-Connect-Backend/server.js
@@ -27,19 +27,17 @@ app.get("/home", (req, res) => {
     res.json("Welcome to Farm Connect Backend");
 });
 
-// Get All Products
+
 app.get("/products", (req, res) => {
     res.json(products);
 });
 
-// Get Product By ID
 app.get("/products/:id", (req, res) => {
     const id = Number(req.params.id);
     const product = products.find((product) => product.id == id);
     res.json(product);
 });
 
-// Add Product
 app.post("/products", (req, res) => {
     const product = req.body;
     products.push(product);
@@ -48,28 +46,7 @@ app.post("/products", (req, res) => {
     });
 });
 
-app.put("/products/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const product = products.find((product) => product.id == id);
-    product.name = req.body.name;
-    product.category = req.body.category;
-    product.price = req.body.price;
-    product.stock = req.body.stock;
-    res.json({
-        message: "Product Updated"
-    });
-});
 
-// Delete Product
-app.delete("/products/:id", (req, res) => {
-    const id = Number(req.params.id);
-    products = products.filter((product) => product.id != id);
-    res.json({
-        message: "Product Deleted"
-    });
-});
-
-// Start Server
 app.listen(8000, () => {
     console.log("Server is started at 8000");
 });
