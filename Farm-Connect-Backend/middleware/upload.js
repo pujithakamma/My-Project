942b5f3949d5ff6eb614585a642d898e79024431
Pujitha Kamma <23ht1a4342@gmail.com>
2026-07-23 02:33:46 +0530

    Added Multer configuration for profile image upload

---
+++ Farm-Connect-Backend/middleware/upload.js
@@ -0,0 +1,33 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only JPG, JPEG and PNG images are allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 2 * 1024 * 1024 // 2 MB
+  }
+});
+
+export default upload;
